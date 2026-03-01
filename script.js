366f6d88096abac1f9f09dd30f8950b16ab7505a
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-03-01 08:57:13 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Sends a console.log when page loads
+window.addEventListener(`load`, pageLoaded)
+function pageLoaded()
+{
+    console.log(`Page loaded`)
+}
+//Function for generating numbers from min to max (includinng min and max)
+function generateRandomNumber(min, max) 
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
