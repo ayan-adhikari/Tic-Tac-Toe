88386905672851d0dd2ed16f6a6f5b31804bba6e
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-03-02 10:49:33 +0800

    Feat: added winner detection for all scenarios

--- script.js
+++ script.js
@@ -32,30 +32,39 @@ for (let i = 1;  i<=9; i++)
   })
 }
 reset.addEventListener(`click`, resetCells)
-//Variable for player
-let player = 1
+//Variables
+let player = 1 //Which player's turn it is
+let winner = 0//Which player won (0 is no one)
 //Function that runs after cell is clicked
 function cellClicked(cell)
 {
-  if (cell.textContent != `X` && cell.textContent != `O`)
+  if (winner == 0)
   {
-    cell.classList.remove(`not-taken`)
-    if (player == 1)
+    if (cell.textContent != `X` && cell.textContent != `O`)
     {
-      cell.textContent = `X`
-      cell.classList.add(`X`)
-      player = 2
+      cell.classList.remove(`not-taken`)
+      if (player == 1)
+      {
+        cell.textContent = `X`
+        cell.classList.add(`X`)
+        player = 2
+      }
+      else if (player == 2)
+      {
+        cell.textContent = `O`
+        cell.classList.add(`O`)
+        player = 1
+      }
     }
-    else if (player == 2)
+    else
     {
-      cell.textContent = `O`
-      cell.classList.add(`O`)
-      player = 1
+      console.log(`${cell} is already taken`)
     }
+    checkWinner()
   }
   else
   {
-    console.log(`${cell} is already taken`)
+    console.log()
   }
 }
 //Reset function
@@ -67,5 +76,296 @@ function resetCells()
     cells[i].classList.add(`not-taken`)
     cells[i].classList.remove(`X`)
     cells[i].classList.remove(`O`)
+    cells[i].classList.remove(`winner`)
   }
+  player = 1
+  winner = 0
+}
+//Function to check winner
+function checkWinner()
+{
+  //Object of winning positions
+  const winningPositions = 
+  {
+    //Horizontal positions
+    horizontal:
+    {
+      position1: [1, 2, 3],
+      position2: [4, 5, 6],
+      position3: [7, 8, 9]
+    },
+    //Vertical positions
+    vertical:
+    {
+      position1: [1, 4, 7],
+      position2: [2, 5, 8],
+      position3: [3, 6, 9],
+    },
+    //Diagonal positions
+    diagonal: 
+    {
+      position1: [1, 5, 9],
+      position2: [3, 5, 7],
+    }
+  }
+//Checking if each winning position has happened for player 1 and 2
+/************************************************************************************/
+  /*
+███  ██  ███      ███   ██  █   █
+ █  █  █ █  █     █  █ █  █ █   █
+ █  █  █ ███      ███  █  █ █   █
+ █  █  █ █        █ █  █  █ █ █ █
+ █   ██  █        █  █  ██   █ █
+*/
+/************************************************************************************/
+  //Very long if statement to detect if player 1 has 3 X's in a row in the top row
+  if (cells[winningPositions.horizontal.position1[0]].textContent == `X` && 
+    cells[winningPositions.horizontal.position1[1]].textContent == `X` && 
+    cells[winningPositions.horizontal.position1[2]].textContent == `X`)
+  {
+    console.log(`Player 1 wins!`)
+    winner = 1
+    //Turn winning cells green
+    cells[winningPositions.horizontal.position1[0]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position1[1]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position1[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  //Very long if statement to detect if player 2 has 3 O's in a row in the top row
+  else if (cells[winningPositions.horizontal.position1[0]].textContent == `O` && 
+    cells[winningPositions.horizontal.position1[1]].textContent == `O` && 
+    cells[winningPositions.horizontal.position1[2]].textContent == `O`)
+  {
+    console.log(`Player 2 wins!`)
+    winner = 2
+    //Turn winning cells green
+    cells[winningPositions.horizontal.position1[0]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position1[1]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position1[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+/*
+█   █ █ ███  ███  █   ████     ███   ██  █   █
+██ ██ █ █  █ █  █ █   █        █  █ █  █ █   █
+█ █ █ █ █  █ █  █ █   ███      ███  █  █ █   █
+█   █ █ █  █ █  █ █   █        █ █  █  █ █ █ █
+█   █ █ ███  ███  ███ ████     █  █  ██   █ █
+*/
+/************************************************************************************/
+  //Very long if statement to detect if player 1 has 3 X's in a row the in middle row
+  else if (cells[winningPositions.horizontal.position2[0]].textContent == `X` && 
+    cells[winningPositions.horizontal.position2[1]].textContent == `X` && 
+    cells[winningPositions.horizontal.position2[2]].textContent == `X`)
+  {
+    console.log(`Player 1 wins!`)
+    winner = 1
+    cells[winningPositions.horizontal.position2[0]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position2[1]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position2[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  //Very long if statement to detect if player 2 has 3 O's in a row the in middle row
+  else if (cells[winningPositions.horizontal.position2[0]].textContent == `O` && 
+    cells[winningPositions.horizontal.position2[1]].textContent == `O` && 
+    cells[winningPositions.horizontal.position2[2]].textContent == `O`)
+  {
+    console.log(`Player 2 wins!`)
+    winner = 2
+    cells[winningPositions.horizontal.position2[0]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position2[1]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position2[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  /*
+  ███   ██  ███ ███  ██  █   █     ███   ██  █   █
+  █  █ █  █  █   █  █  █ ██ ██     █  █ █  █ █   █
+  ███  █  █  █   █  █  █ █ █ █     ███  █  █ █   █
+  █  █ █  █  █   █  █  █ █   █     █ █  █  █ █ █ █
+  ███   ██   █   █   ██  █   █     █  █  ██   █ █
+  */
+/************************************************************************************/
+  //Very long if statement to detect if player 1 has 3 X's in a row the in bottom row
+  else if (cells[winningPositions.horizontal.position3[0]].textContent == `X` && 
+    cells[winningPositions.horizontal.position3[1]].textContent == `X` && 
+    cells[winningPositions.horizontal.position3[2]].textContent == `X`)
+  {
+    console.log(`Player 1 wins!`)
+    winner = 1
+    cells[winningPositions.horizontal.position3[0]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position3[1]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position3[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  //Very long if statement to detect if player 2 has 3 O's in a row the in bottom row
+  else if (cells[winningPositions.horizontal.position3[0]].textContent == `O` && 
+    cells[winningPositions.horizontal.position3[1]].textContent == `O` && 
+    cells[winningPositions.horizontal.position3[2]].textContent == `O`)
+  {
+    console.log(`Player 2 wins!`)
+    winner = 2
+    cells[winningPositions.horizontal.position3[0]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position3[1]].classList.add(`winner`)
+    cells[winningPositions.horizontal.position3[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  /*
+   ███  ██  █   █  █ █   █ █  █      ██  █  █ ████
+  █    █  █ █   █  █ ██ ██ ██ █     █  █ ██ █ █
+  █    █  █ █   █  █ █ █ █ █ ██     █  █ █ ██ ███
+  █    █  █ █   █  █ █   █ █  █     █  █ █  █ █
+   ███  ██  ███ ████ █   █ █  █      ██  █  █ ████
+  */
+/************************************************************************************/
+  //Very long if statement to detect if player 1 has 3 X's in a row the in the left column (column 1)
+  else if (cells[winningPositions.vertical.position1[0]].textContent == `X` && 
+    cells[winningPositions.vertical.position1[1]].textContent == `X` && 
+    cells[winningPositions.vertical.position1[2]].textContent == `X`)
+  {
+    console.log(`Player 1 wins!`)
+    winner = 1
+    cells[winningPositions.vertical.position1[0]].classList.add(`winner`)
+    cells[winningPositions.vertical.position1[1]].classList.add(`winner`)
+    cells[winningPositions.vertical.position1[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  //Very long if statement to detect if player 2 has 3 O's in a row the in the left column (column 1)
+  else if (cells[winningPositions.vertical.position1[0]].textContent == `O` && 
+    cells[winningPositions.vertical.position1[1]].textContent == `O` && 
+    cells[winningPositions.vertical.position1[2]].textContent == `O`)
+  {
+    console.log(`Player 2 wins!`)
+    winner = 2
+    cells[winningPositions.vertical.position1[0]].classList.add(`winner`)
+    cells[winningPositions.vertical.position1[1]].classList.add(`winner`)
+    cells[winningPositions.vertical.position1[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  /*
+   ███  ██  █   █  █ █   █ █  █     ███ █   █  ██
+  █    █  █ █   █  █ ██ ██ ██ █      █  █   █ █  █
+  █    █  █ █   █  █ █ █ █ █ ██      █  █   █ █  █
+  █    █  █ █   █  █ █   █ █  █      █  █ █ █ █  █
+   ███  ██  ███ ████ █   █ █  █      █   █ █   ██
+  */
+/************************************************************************************/
+  //Very long if statement to detect if player 1 has 3 X's in a row the in the middle column (column 2)
+  else if (cells[winningPositions.vertical.position2[0]].textContent == `X` && 
+    cells[winningPositions.vertical.position2[1]].textContent == `X` && 
+    cells[winningPositions.vertical.position2[2]].textContent == `X`)
+  {
+    console.log(`Player 1 wins!`)
+    winner = 1
+    cells[winningPositions.vertical.position2[0]].classList.add(`winner`)
+    cells[winningPositions.vertical.position2[1]].classList.add(`winner`)
+    cells[winningPositions.vertical.position2[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  //Very long if statement to detect if player 2 has 3 O's in a row the in the middle column (column 2)
+  else if (cells[winningPositions.vertical.position2[0]].textContent == `O` && 
+    cells[winningPositions.vertical.position2[1]].textContent == `O` && 
+    cells[winningPositions.vertical.position2[2]].textContent == `O`)
+  {
+    console.log(`Player 2 wins!`)
+    winner = 2
+    cells[winningPositions.vertical.position2[0]].classList.add(`winner`)
+    cells[winningPositions.vertical.position2[1]].classList.add(`winner`)
+    cells[winningPositions.vertical.position2[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  /*
+   ███  ██  █   █  █ █   █ █  █     ███ █  █ ███  ████ ████
+  █    █  █ █   █  █ ██ ██ ██ █      █  █  █ █  █ █    █
+  █    █  █ █   █  █ █ █ █ █ ██      █  ████ ███  ███  ███
+  █    █  █ █   █  █ █   █ █  █      █  █  █ █ █  █    █
+   ███  ██  ███ ████ █   █ █  █      █  █  █ █  █ ████ ████
+  */
+/************************************************************************************/
+  //Very long if statement to detect if player 1 has 3 X's in a row the in the right column (column 3)
+  else if (cells[winningPositions.vertical.position3[0]].textContent == `X` && 
+    cells[winningPositions.vertical.position3[1]].textContent == `X` && 
+    cells[winningPositions.vertical.position3[2]].textContent == `X`)
+  {
+    console.log(`Player 1 wins!`)
+    winner = 1
+    cells[winningPositions.vertical.position3[0]].classList.add(`winner`)
+    cells[winningPositions.vertical.position3[1]].classList.add(`winner`)
+    cells[winningPositions.vertical.position3[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  //Very long if statement to detect if player 2 has 3 O's in a row the in the right column (column 3)
+  else if (cells[winningPositions.vertical.position3[0]].textContent == `O` && 
+    cells[winningPositions.vertical.position3[1]].textContent == `O` && 
+    cells[winningPositions.vertical.position3[2]].textContent == `O`)
+  {
+    console.log(`Player 2 wins!`)
+    winner = 2
+    cells[winningPositions.vertical.position3[0]].classList.add(`winner`)
+    cells[winningPositions.vertical.position3[1]].classList.add(`winner`)
+    cells[winningPositions.vertical.position3[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  /*
+  ███  █  ██   ███  ██  █  █  ██  █        ██  █  █ ████
+  █  █ █ █  █ █    █  █ ██ █ █  █ █       █  █ ██ █ █
+  █  █ █ ████ █ ██ █  █ █ ██ ████ █       █  █ █ ██ ███
+  █  █ █ █  █ █  █ █  █ █  █ █  █ █       █  █ █  █ █
+  ███  █ █  █  ███  ██  █  █ █  █ ███      ██  █  █ ████
+  (Top left to bottom right)*/
+/************************************************************************************/
+  //Very long if statement to detect if player 1 has 3 X's in a row the in diagonal one (top left to bottom right)
+  else if (cells[winningPositions.diagonal.position1[0]].textContent == `X` && 
+    cells[winningPositions.diagonal.position1[1]].textContent == `X` && 
+    cells[winningPositions.diagonal.position1[2]].textContent == `X`)
+  {
+    console.log(`Player 1 wins!`)
+    winner = 1
+    cells[winningPositions.diagonal.position1[0]].classList.add(`winner`)
+    cells[winningPositions.diagonal.position1[1]].classList.add(`winner`)
+    cells[winningPositions.diagonal.position1[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  //Very long if statement to detect if player 2 has 3 O's in a row the in diagonal one (top left to bottom right)
+  else if (cells[winningPositions.diagonal.position1[0]].textContent == `O` && 
+    cells[winningPositions.diagonal.position1[1]].textContent == `O` && 
+    cells[winningPositions.diagonal.position1[2]].textContent == `O`)
+  {
+    console.log(`Player 2 wins!`)
+    winner = 2
+    cells[winningPositions.diagonal.position1[0]].classList.add(`winner`)
+    cells[winningPositions.diagonal.position1[1]].classList.add(`winner`)
+    cells[winningPositions.diagonal.position1[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  /*
+  ███  █  ██   ███  ██  █  █  ██  █       ███ █   █  ██
+  █  █ █ █  █ █    █  █ ██ █ █  █ █        █  █   █ █  █
+  █  █ █ ████ █ ██ █  █ █ ██ ████ █        █  █   █ █  █
+  █  █ █ █  █ █  █ █  █ █  █ █  █ █        █  █ █ █ █  █
+  ███  █ █  █  ███  ██  █  █ █  █ ███      █   █ █   ██
+  (Top right to bottom left)*/
+/************************************************************************************/
+  //Very long if statement to detect if player 1 has 3 X's in a row the in diagonal two (top right to bottom left)
+  else if (cells[winningPositions.diagonal.position2[0]].textContent == `X` && 
+    cells[winningPositions.diagonal.position2[1]].textContent == `X` && 
+    cells[winningPositions.diagonal.position2[2]].textContent == `X`)
+  {
+    console.log(`Player 1 wins!`)
+    winner = 1
+    cells[winningPositions.diagonal.position2[0]].classList.add(`winner`)
+    cells[winningPositions.diagonal.position2[1]].classList.add(`winner`)
+    cells[winningPositions.diagonal.position2[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
+  //Very long if statement to detect if player 2 has 3 O's in a row the in diagonal two (top right to bottom left)
+  else if (cells[winningPositions.diagonal.position2[0]].textContent == `O` && 
+    cells[winningPositions.diagonal.position2[1]].textContent == `O` && 
+    cells[winningPositions.diagonal.position2[2]].textContent == `O`)
+  {
+    console.log(`Player 2 wins!`)
+    winner = 2
+    cells[winningPositions.diagonal.position2[0]].classList.add(`winner`)
+    cells[winningPositions.diagonal.position2[1]].classList.add(`winner`)
+    cells[winningPositions.diagonal.position2[2]].classList.add(`winner`)
+  }
+/************************************************************************************/
 }
